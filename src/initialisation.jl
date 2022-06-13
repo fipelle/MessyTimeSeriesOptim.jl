f86d0c5314a554e28be920c699e96c8ae4c9bfdb
fipelle <6272230+fipelle@users.noreply.github.com>
2022-06-13 18:14:30 +0100

    Increased P0 scaling factor to 1e5

--- src/initialisation.jl
+++ src/initialisation.jl
@@ -177,7 +177,7 @@ function initial_univariate_decomposition(data::JVector{Float64}, lags::Int64, Î
     # Initial conditions (covariance)
     C_cycle = C[3:end, 3:end];
     DQD_cycle = Symmetric(cat(dims=[1,2], Q[2+is_llt, 2+is_llt], zeros(lags-1, lags-1)));
-    P0_trend = Symmetric(1e4*Matrix(I, 2, 2));
+    P0_trend = Symmetric(1e5*Matrix(I, 2, 2));
     P0 = Symmetric(cat(dims=[1,2], P0_trend, solve_discrete_lyapunov(C_cycle, DQD_cycle).data));
 
     # Set KalmanSettings
