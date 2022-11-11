163d3b4bed772056682309cfd3a117ca1ca518b6
fipelle <6272230+fipelle@users.noreply.github.com>
2022-11-11 04:22:39 +0000

    Minor promotion fix

--- src/models/dfm.jl
+++ src/models/dfm.jl
@@ -503,7 +503,7 @@ function initialise(estim::DFMSettings, trends_skeleton::FloatMatrix)
     for (i, j) in enumerate(coordinates_non_deterministic_trends);        
         for k in ifelse(estim.drifts_selection[i], j-2:j+1, j:j+1) # changing j-2:j+1 to j:j+1 and removing the comment at line #511 allows to initialise to zero the relevant part of P0 associated with deterministic trends
             # Something like `X0_trends[k] = some value` could be used here for an ad-hoc initialisation of `X0_trends`
-            P0_trends[k, k] = 10^floor(Int, 2+log10(common_trends[i, 1]^2));
+            P0_trends[k, k] = 10.0^floor(Int, 2+log10(common_trends[i, 1]^2));
         end
     end
     
