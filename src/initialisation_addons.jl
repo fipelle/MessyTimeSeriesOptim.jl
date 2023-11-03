a67d2b0b5febdf7604cd104df334087946eb942a
fipelle <6272230+fipelle@users.noreply.github.com>
2023-11-03 21:09:29 +0000

    Added initialisation addon

---
+++ src/initialisation_addons.jl
@@ -0,0 +1,65 @@
+function initial_sspace_structure(
+    data::Union{FloatMatrix, JMatrix{Float64}},
+    estim::EstimSettings
+)
+
+    # `n` for initialisation (it may differ from the one in `estim`)
+    n_series_in_data = size(data, 1);
+
+    # Setup loading structure for the trends (common and idiosyncratic)
+    B_trends = kron(estim.trends_skeleton, [1.0 0.0]);
+
+    # Setup loadings structure for the idiosyncratic cycles
+    B_idio_cycles = Matrix(I, n_series_in_data, n_series_in_data) |> Matrix{Float64};
+
+    # Setup loading structure for the cycles, employing the relevant identification scheme
+    B_common_cycles = kron(estim.cycles_skeleton, ones(1, estim.lags));
+    B_common_cycles[1, :] .= 0.0;
+    for i in [1+(i-1)*estim.lags for i in 1:estim.n_cycles]
+        B_common_cycles[1, i] = 1.0;
+    end
+
+    # Setup loading matrix
+    B = hcat(B_trends, B_idio_cycles, B_common_cycles);
+    coordinates_free_params_B = B .!= 1.0;
+
+    # Setup covariance matrix measurement error
+    R = ε*I;
+
+    # Setup transition matrix for the trends (common and idiosyncratic)
+    C_trends_template = [2.0 -1.0; 1.0 0.0];
+    C_trends = cat(dims=[1,2], [C_trends_template for i in 1:estim.n_trends]...);
+
+    # Setup transition matrix for the idiosyncratic cycles
+    C_idio_cycles = 0.1*Matrix(I, n_series_in_data, n_series_in_data);
+
+    # Setup transition matrix for the common cycles
+    C_common_cycles_template = companion_form([0.9 zeros(1, estim.lags-1)], extended=false);
+    C_common_cycles = cat(dims=[1,2], [C_common_cycles_template for i in 1:estim.n_cycles]...);
+
+    # Setup transition matrix
+    C = cat(dims=[1,2], C_trends, C_idio_cycles, C_common_cycles);
+    coordinates_free_params_C = (C .!= 0.1) .& (C .!= 1.0) .& (C .!= 0.0);
+
+    # Setup covariance matrix of the states' innovation
+    # NOTE: all diagonal elements are estimated during the initialisation
+    Q = Symmatric(Matrix(I, estim.n_trends + n_series_in_data + estim.n_cycles, estim.n_trends + n_series_in_data + estim.n_cycles));
+
+    # Setup selection matrix D for the trends
+    D_trends_template = [1.0; 0.0];
+    D_trends = cat(dims=[1,2], [D_trends_template for i in 1:estim.n_trends]...);
+
+    # Setup selection matrix D for idiosyncratic cycles
+    D_idio_cycles = Matrix(I, n_series_in_data, n_series_in_data);
+
+    # Setup selection matrix D for the common cycles
+    D_common_cycles_template = zeros(estim.lags);
+    D_common_cycles_template[1] = 1.0;
+    D_common_cycles = cat(dims=[1,2], [D_common_cycles_template for i in 1:estim.n_cycles]...);
+
+    # Setup selection matrix D
+    D = cat(dims=[1,2], D_trends, D_idio_cycles, D_common_cycles);
+
+    # Setup initial conditions for the trends
+    # -> TBA (loop over trends to initialise on the mean of the data w common components)
+end
