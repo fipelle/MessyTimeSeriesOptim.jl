197a398e8c7733572052a296af3336889dbd0f0a
fipelle <6272230+fipelle@users.noreply.github.com>
2023-11-07 15:06:33 +0000

    Implemented changes in NLOpt interface

--- src/initialisation.jl
+++ src/initialisation.jl
@@ -31,11 +31,11 @@ function update_sspace_DQD_and_P0_from_params!(coordinates_free_params_P0::BitMa
 end
 
 """
-    fmin!(constrained_params::Vector{Float64}, sspace::KalmanSettings)
+    fmin!(constrained_params::Vector{Float64}, sspace::KalmanSettings, coordinates_free_params_B::BitMatrix, coordinates_free_params_P0::BitMatrix)
 
 Return -1 times the log-likelihood function (or a large number in case of numerical problems).
 """
-function fmin!(constrained_params::Vector{Float64}, sspace::KalmanSettings)
+function fmin!(constrained_params::Vector{Float64}, sspace::KalmanSettings, coordinates_free_params_B::BitMatrix, coordinates_free_params_P0::BitMatrix)
 
     # Update sspace accordingly
     update_sspace_B_from_params!(constrained_params, coordinates_free_params_B, sspace);
@@ -85,186 +85,6 @@ end
 APIs to call `fmin!` with Tuple parameters.
 """
 call_fmin!(constrained_params::Vector{Float64}, tuple_fmin_args::Tuple) = fmin!(constrained_params, tuple_fmin_args...);
-
-"""
-    call_reparametrised_fmin!(params::Vector{Float64}, tuple_args::Tuple)
-
-APIs to call `fmin!` after transforming the candidate parameters so that their support is unbounded.
-"""
-function call_reparametrised_fmin!(params::Vector{Float64}, tuple_args::Tuple)
-
-    params_lb = tuple_args[1];
-    params_ub = tuple_args[2];
-    tuple_fmin_args = tuple_args[3:end];
-
-    # Unconstrained -> constrained parameters
-    constrained_params = similar(params);
-    for i in axes(constrained_params, 1)
-        constrained_params[i] = get_bounded_logit(params[i], params_lb[i], params_ub[i]);
-    end
-
-    return call_fmin!(constrained_params, tuple_fmin_args);
-end
-
-"""
-    initial_univariate_decomposition(data::Union{FloatVector, JVector{Float64}}, lags::Int64, ε::Float64, is_rw_trend::Bool, is_llt::Bool; sigma_lb::Vector{Float64}=[1e-3; 1e3], sigma_ub::Vector{Float64}=[1e3; 1e4])
-
-This function returns an initial estimate of the non-stationary and stationary components of each series. In doing so, it provides a rough starting point for the ECM algorithm.
-
-# Note
-- If both `is_rw_trend` and `is_llt` are false this function models the trend as in Kitagawa and Gersch (1996, ch. 8).
-"""
-function initial_univariate_decomposition(data::Union{FloatVector, JVector{Float64}}, lags::Int64, ε::Float64, is_rw_trend::Bool, is_llt::Bool; sigma_lb::Vector{Float64}=[1e-3; 1e3], sigma_ub::Vector{Float64}=[1e3; 1e4])
-
-    if is_rw_trend && is_llt
-        error("Either is_rw_trend or is_llt can be true");
-    end
-
-    # Measurement equation coefficients
-    B = [1.0 0.0 1.0 zeros(1, lags-1)];
-    R = ε*I;
-
-    # Drift-less random walk
-    if is_rw_trend
-        C_trend = [1.0 0.0; 1.0 0.0]; # this is not the most compressed representation, but simplifies the remaining part of this function without significantly compromising the run time
-    
-    # Local linear trend or Kitagawa second order trend (special case of the local linear trend)
-    else
-        C_trend = [1.0 1.0; 0.0 1.0];
-    end
-
-    # Stationary dynamics
-    C = cat(dims=[1,2], C_trend, companion_form([0.9 zeros(1, lags-1)], extended=false));
-
-    # Remaining transition equation coefficients
-    D = zeros(2+lags, 2+is_llt);
-    
-    # Drift-less random walk
-    if is_rw_trend
-        D[1,1] = 1.0;
-        D[3,2] = 1.0;
-
-    # Local linear trend
-    elseif is_llt
-        D[1,1] = 1.0;
-        D[2,2] = 1.0;
-        D[3,3] = 1.0;
-
-    # Kitagawa second order trend (special case of the local linear trend)
-    else
-        D[2,1] = 1.0;
-        D[3,2] = 1.0;
-    end
-
-    # Covariance matrix of the transition innovations
-    Q = Symmetric(1.0*Matrix(I, 2+is_llt, 2+is_llt));
-
-    # Initial conditions (mean)
-    X0 = zeros(2+is_llt+lags);
-
-    # Initial conditions (covariance)
-    C_cycle = C[3:end, 3:end];
-    DQD_cycle = Symmetric(cat(dims=[1,2], Q[2+is_llt, 2+is_llt], zeros(lags-1, lags-1)));
-    P0_cycle = solve_discrete_lyapunov(C_cycle, DQD_cycle).data;
-    P0_trend = Symmetric(Inf*Matrix(I, 2, 2));
-    
-    # Trend initialisation
-    X0[1] = first(skipmissing(data)); # this allows for a weakly diffuse initialisation of the trend
-    P0_trend[isinf.(P0_trend)] .= 10.0^floor(Int, 1+log10(abs(first(skipmissing(data)))));
-    
-    # Merge into `P0`
-    P0 = Symmetric(cat(dims=[1,2], P0_trend, P0_cycle));
-    
-    # Set KalmanSettings
-    sspace = KalmanSettings(Array(data'), B, R, C, D, Q, X0, P0, compute_loglik=true);
-
-    #=
-    Initial values / bounds for the parameters
-
-    When is_llt == false:
-    - sigma_{drift}^2
-    - sigma_{cycle}^2 / sigma_{drift}^2
-    - AR(p) parameters
-
-    When is_llt == true:
-    - sigma_{drift}^2
-    - sigma_{trend}^2 / sigma_{drift}^2
-    - sigma_{cycle}^2 / sigma_{drift}^2
-    - AR(p) parameters
-
-    In the case in which is_rw_trend == true, sigma_{drift}^2 denotes the variance of the trend.
-    =#
-
-    params_0  = [(sigma_lb + sigma_ub)/2; 0.90; zeros(lags-1)];
-    params_lb = [sigma_lb; -1*ones(lags)];
-    params_ub = [sigma_ub; +1*ones(lags)];
-    
-    # Add `sigma_{trend}^2 / sigma_{drift}^2` entries
-    if is_llt
-        insert!(params_0,  2, (sigma_lb[1] + sigma_ub[1])/2);
-        insert!(params_lb, 2, sigma_lb[1]);
-        insert!(params_ub, 2, sigma_ub[1]);
-    end
-    
-    # Best derivative-free option from NLopt -> NLopt.LN_SBPLX()
-
-    # Maximum likelihood
-    tuple_fmin_args = (is_llt, sspace);
-    prob = OptimizationFunction(call_fmin!);
-    prob = OptimizationProblem(prob, params_0, tuple_fmin_args, lb=params_lb, ub=params_ub);
-    res_optim = solve(prob, NLopt.LN_SBPLX(), abstol=0.0, reltol=1e-3);
-    minimizer_bounded = res_optim.u;
-
-    #=
-    # Alternative way to handle the bounds in the optimisation
-    tuple_fmin_args = (params_lb, params_ub, is_llt, sspace);
-    params_0_unb = [get_unbounded_logit(params_0[i], params_lb[i], params_ub[i]) for i in axes(params_0, 1)];
-    prob = OptimizationFunction(call_reparametrised_fmin!);
-    prob = OptimizationProblem(prob, params_0_unb, tuple_fmin_args);
-    res_optim = solve(prob, NLopt.LN_SBPLX(), abstol=0.0, reltol=1e-3);
-    minimizer_bounded = [get_bounded_logit(res_optim.u[i], params_lb[i], params_ub[i]) for i in axes(res_optim.u, 1)];
-    =#
-    
-    # Update sspace accordingly
-    update_sspace_Q_from_params!(minimizer_bounded, is_llt, sspace);
-    update_sspace_C_from_params!(minimizer_bounded, is_llt, sspace);
-    update_sspace_DQD_and_P0_from_params!(sspace);
-    
-    # Retrieve optimal states
-    status = kfilter_full_sample(sspace);
-    smoothed_states, _ = ksmoother(sspace, status);
-    smoothed_states_matrix = hcat(smoothed_states...);
-    trend = smoothed_states_matrix[1, :];
-    drift_or_trend_lagged = smoothed_states_matrix[2, :];
-    cycle = smoothed_states_matrix[3, :];
-
-    # Return output
-    return trend, drift_or_trend_lagged, cycle, minimizer_bounded, status;
-end
-
-"""
-    initial_univariate_decomposition_kitagawa(data::Union{FloatVector, JVector{Float64}}, lags::Int64, ε::Float64, is_rw_trend::Bool; sigma_lb::Vector{Float64}=[1e-3; 1e3], sigma_ub::Vector{Float64}=[1e3; 1e4])
-
-This function returns an initial estimate of the non-stationary and stationary components of each series. In doing so, it provides a rough starting point for the ECM algorithm.
-
-If `is_rw_trend` is false this function models the trend as in Kitagawa and Gersch (1996, ch. 8).
-"""
-function initial_univariate_decomposition_kitagawa(data::Union{FloatVector, JVector{Float64}}, lags::Int64, ε::Float64, is_rw_trend::Bool; sigma_lb::Vector{Float64}=[1e-3; 1e3], sigma_ub::Vector{Float64}=[1e3; 1e4])
-    trend, _, cycle, minimizer_bounded, _ = initial_univariate_decomposition(data, lags, ε, is_rw_trend, false, sigma_lb=sigma_lb, sigma_ub=sigma_ub);
-    return trend, cycle, minimizer_bounded;
-end
-
-"""
-    initial_univariate_decomposition_llt(data::Union{FloatVector, JVector{Float64}}, lags::Int64, ε::Float64, is_rw_trend::Bool; sigma_lb::Vector{Float64}=[1e-3; 1e3], sigma_ub::Vector{Float64}=[1e3; 1e4])
-
-This function returns an initial estimate of the non-stationary and stationary components of each series. In doing so, it provides a rough starting point for the ECM algorithm.
-
-If `is_rw_trend` is false this function models the trend as a local linear trend.
-"""
-function initial_univariate_decomposition_llt(data::Union{FloatVector, JVector{Float64}}, lags::Int64, ε::Float64, is_rw_trend::Bool; sigma_lb::Vector{Float64}=[1e-3; 1e3], sigma_ub::Vector{Float64}=[1e3; 1e4])
-    trend, drift, cycle, minimizer_bounded, _ = initial_univariate_decomposition(data, lags, ε, is_rw_trend, true, sigma_lb=sigma_lb, sigma_ub=sigma_ub);
-    return trend, drift, cycle, minimizer_bounded;
-end
 
 """
     initial_sspace_structure(data::Union{FloatMatrix, JMatrix{Float64}}, estim::EstimSettings)
@@ -399,8 +219,30 @@ function initial_detrending(Y_untrimmed::Union{FloatMatrix, JMatrix{Float64}}, e
     # Set KalmanSettings
     sspace = KalmanSettings(Array(data'), B, R, C, D, Q, X0, P0, compute_loglik=true);
 
+    # Initial guess for the parameters
+    params_0  = ones(sum(coordinates_free_params_B)+size(Q, 1));
+    params_lb = params_0/1000
+    params_ub = params_0*1000
+    
+    # Best derivative-free option from NLopt -> NLopt.LN_SBPLX()
 
+    # Maximum likelihood
+    tuple_fmin_args = (sspace, coordinates_free_params_B, coordinates_free_params_P0);
+    prob = OptimizationFunction(call_fmin!);
+    prob = OptimizationProblem(prob, params_0, tuple_fmin_args, lb=params_lb, ub=params_ub);
+    res_optim = solve(prob, NLopt.LN_SBPLX(), abstol=0.0, reltol=1e-3);
+    minimizer_bounded = res_optim.u;
 
+    # Update sspace accordingly
+    update_sspace_B_from_params!(minimizer_bounded, coordinates_free_params_B, sspace);
+    update_sspace_Q_from_params!(minimizer_bounded, coordinates_free_params_B, sspace);
+    update_sspace_DQD_and_P0_from_params!(coordinates_free_params_P0, sspace);
+    
+    # Retrieve optimal states
+    status = kfilter_full_sample(sspace);
+    smoothed_states, _ = ksmoother(sspace, status);
+    smoothed_states_matrix = hcat(smoothed_states...);
+        
     # Return output
-    return common_trends, trends_variance, detrended_data;
+    return smoothed_states_matrix;
 end
